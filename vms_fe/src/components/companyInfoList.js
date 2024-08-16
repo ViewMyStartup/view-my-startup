232933cb10a20c91bafaa3d9701117b8ebdaa495
S2OHEE <s2h110@naver.com>
2024-08-16 17:38:12 +0900

    ✅ [Test] 테스트를 위해 기업1에 company_logo_1.svg 이미지 추가

--- vms_fe/src/components/companyInfoList.js
+++ vms_fe/src/components/companyInfoList.js
@@ -1,10 +1,11 @@
 //여러 개의 CompanyItem 컴포넌트를 포함하는 컴포넌트
 import React from "react";
 import CompanyItem from "./companyItem.js";
+import companyLogo1 from "../assets/images/company_logo_1.svg";
 
 // 테스트를 위해 mock데이터 넣음
 const companies = [
-  { logo: "logo1.png", name: "기업명 1", category: "에듀테크" },
+  { logo: companyLogo1, name: "코드잇", category: "에듀테크" },
   { logo: "logo2.png", name: "기업명 2", category: "핀테크" },
 ];
 
