59409834bdb3c647787423dcbbd50f906608faee
S2OHEE <s2h110@naver.com>
2024-08-16 15:01:19 +0900

    :sparkles: [Feat] 기업 선택하는 리스트 컴포넌트 추가

---
+++ vms_fe/src/components/companyInfoList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CompanyListSelect = () => {
+  return (
+    <div>
+      <div className="companyLogoName">기업로고 기업명 카테고리</div>
+      <button className="select">선택하기</button>
+    </div>
+  );
+};
