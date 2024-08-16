d5f5f11a42ebc4ebb130f68b5044259b4cf71474
S2OHEE <s2h110@naver.com>
2024-08-16 11:28:39 +0900

    :sparkles: [Feat] inputBar 상태 및 상태관리 변수 추가

--- vms_fe/src/components/inputBar.js
+++ vms_fe/src/components/inputBar.js
@@ -1,5 +1,15 @@
 import React, { useState } from "react";
 
-const InputBar = () => {};
+const InputBar = () => {
+  // 상태 관리
+  const [investorName, setInvestorName] = useState(""); // 투자자 이름
+  const [investmentAmount, setInvestmentAmount] = useState(""); // 투자 금액
+  const [investmentComment, setInvestmentComment] = useState(""); // 투자 코멘트
+  const [password, setPassword] = useState(""); // 비밀번호
+  const [confirmPassword, setConfirmPassword] = useState(""); // 비밀번호 확인
+  const [errors, setErrors] = useState({}); // 에러 처리
+
+  return <div className={styles.inputFormContainer}></div>;
+};
 
 export default InputBar;
