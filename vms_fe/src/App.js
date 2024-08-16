6dd9582870b04e6592a5401496a34ffda9dba77b
Jun5 <chdh1005@gmail.com>
2024-08-16 12:00:46 +0900

    :white_check_mark: [Test] App.js 내 CompanyPerRow & HeaderColumns 컴포넌트 테스트 코드 추가

--- vms_fe/src/App.js
+++ vms_fe/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import "./styles/reset.css";
 import React, { useState } from "react";
+
+// 컴포넌트
 import Pagination from "./components/Pagination.js";
+import HeaderColumns from "./components/HeaderColumns";
+import CompanyDataPerRow from "./components/CompanyDataPerRow";
+
+//테스트용 이미지
+import Companyimg from "./assets/images/mock_img/company_temp.svg";
 
 const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -12,7 +19,33 @@ const App = () => {
       setCurrentPage(page);
     }
   };
-  
+
+  // CompanyPerRow & HeaderColumns 컴포넌트 테스트용 데이터 데이터
+  const data = {
+    rank: 1,
+    name: "에듀넥스트",
+    img: Companyimg,
+    description:
+      "코드잇은 ‘온라인 코딩 교육 서비스’를 운영하는 EdTech 스타트업입니다. 코딩 교육에 대한 수...",
+    category: "에듀테크",
+    total_investment: 5000000,
+    revenue: 12000000,
+    employees: 50,
+  };
+
+  const vmsData = {
+    total_investment_vms: 500000000,
+    total_investment_infact: 120000000000,
+  };
+
+  const userData = {
+    userName: "정준호",
+    userRank: 1,
+    user_total_investment: 9999000000,
+    user_comment: "너무 어려워요",
+  };
+
+  // 
   return (
     <div>
       <h1>Pagination test</h1>
@@ -22,10 +55,18 @@ const App = () => {
         onPageChange={handlePageChange}
         hasNext={currentPage < totalPages}
       />
+      <h1>CompanyPerRow & HeaderColumns 컴포넌트 테스트</h1>
+      <HeaderColumns />
+      <CompanyDataPerRow type="rank" companyData={data} />
+      <HeaderColumns type="noRank" />
+      <CompanyDataPerRow type="noRank" companyData={data} />
+      <HeaderColumns type="invest" />
+      <CompanyDataPerRow type="invest" companyData={data} vmsData={vmsData} />
+      <HeaderColumns type="comment" />
+      <CompanyDataPerRow type="comment" userData={userData} />
     </div>
   );
 };
 
 export default App;
-
 
