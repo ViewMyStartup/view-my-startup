a772653a385c1b450095c3b0c93df457f55aa05c
S2OHEE <s2h110@naver.com>
2024-08-16 11:41:54 +0900

    :sparkles: [Feat] 투자자 이름 input 및 핸들러, placeholder 등 추가

--- vms_fe/src/components/inputBar.js
+++ vms_fe/src/components/inputBar.js
@@ -8,15 +8,31 @@ const InputBar = () => {
   const [confirmPassword, setConfirmPassword] = useState(""); // 비밀번호 확인
   const [errors, setErrors] = useState({}); // 에러 처리
 
-  return;
-  <div className={styles.inputFormContainer}>
-    <div className={styles.inputFormTop}>
-      <h1 className={styles.h1}>기업에 투자하기</h1>
-      <button className={styles.inputTopX}>X</button>
+  return (
+    <div className={styles.inputFormContainer}>
+      <div className={styles.inputFormTop}>
+        <h1 className={styles.h1}>기업에 투자하기</h1>
+        <button className={styles.inputTopX}>X</button>
+      </div>
+      <h2 className={styles.h2}>투자 기업 정보</h2>
+      <p>(기업아이콘) 기업명: 여기는 기업 정보 데이터 불러와야함</p>
+      <form onSubmit={handleSubmit} className={styles.inputForm}>
+        <div className={styles.formGroup}>
+          <label htmlFor="investorName" className={styles.label}>
+            투자자 이름
+          </label>
+          <input
+            type="text"
+            id="investorName"
+            placeholder="투자자 이름을 입력해주세요"
+            value={investorName}
+            onChange={(e) => setInvestorName(e.target.value)}
+            className={styles.input}
+          />
+        </div>
+      </form>
     </div>
-    <h2 className={styles.h2}>투자 기업 정보</h2>
-    <p>(기업아이콘) 기업명: 여기는 기업 정보 데이터 불러와야함</p>
-  </div>;
+  );
 };
 
 export default InputBar;
