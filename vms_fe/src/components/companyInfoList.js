8e26047846d272d3c6143f6ca3b1c1c52963fd42
S2OHEE <s2h110@naver.com>
2024-08-16 16:02:55 +0900

    ✅ [Test] 테스트를 위한 mock 데이터 및 파일 맨 위 컴포넌트 설명 주석 추가

--- vms_fe/src/components/companyInfoList.js
+++ vms_fe/src/components/companyInfoList.js
@@ -1,5 +1,13 @@
+//여러 개의 CompanyItem 컴포넌트를 포함하는 컴포넌트
+
 import React from "react";
 import CompanyItem from "./companyItem.Js";
+
+// 테스트를 위해 mock데이터 넣음
+const companies = [
+  { logo: "logo1.png", name: "기업명 1", category: "에듀테크" },
+  { logo: "logo2.png", name: "기업명 2", category: "핀테크" },
+];
 
 const CompanyListSelect = () => {
   return (
