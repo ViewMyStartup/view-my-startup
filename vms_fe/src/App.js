a85787adcb431e05f3c7d2e3985e0913ddde839d
S2OHEE <s2h110@naver.com>
2024-08-16 15:53:24 +0900

    :wrench: [Chore] modify App.js for testing component

--- vms_fe/src/App.js
+++ vms_fe/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./styles/reset.css";
-import React, { useState } from "react";
+import React from "react";
 
 // 컴포넌트
 import CompanyListSelect from "./components/companyInfoList.js";
@@ -8,6 +8,7 @@ import CompanyListSelect from "./components/companyInfoList.js";
 const App = () => {
   return (
     <div>
+      <h1>기업 리스트 test</h1>
       <CompanyListSelect />
     </div>
   );
