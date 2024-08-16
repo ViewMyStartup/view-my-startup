f4f92837c4ca02c8ca271db8a87f143c9af399da
S2OHEE <s2h110@naver.com>
2024-08-16 11:35:07 +0900

    :sparkles: [Feat] 임시로 inputFrom 상단 문구 추가해놓음

--- vms_fe/src/components/inputBar.js
+++ vms_fe/src/components/inputBar.js
@@ -4,12 +4,19 @@ const InputBar = () => {
   // 상태 관리
   const [investorName, setInvestorName] = useState(""); // 투자자 이름
   const [investmentAmount, setInvestmentAmount] = useState(""); // 투자 금액
-  const [investmentComment, setInvestmentComment] = useState(""); // 투자 코멘트
   const [password, setPassword] = useState(""); // 비밀번호
   const [confirmPassword, setConfirmPassword] = useState(""); // 비밀번호 확인
   const [errors, setErrors] = useState({}); // 에러 처리
 
-  return <div className={styles.inputFormContainer}></div>;
+  return;
+  <div className={styles.inputFormContainer}>
+    <div className={styles.inputFormTop}>
+      <h1 className={styles.h1}>기업에 투자하기</h1>
+      <button className={styles.inputTopX}>X</button>
+    </div>
+    <h2 className={styles.h2}>투자 기업 정보</h2>
+    <p>(기업아이콘) 기업명: 여기는 기업 정보 데이터 불러와야함</p>
+  </div>;
 };
 
 export default InputBar;
