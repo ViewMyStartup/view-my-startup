1cb8998b60d582979fd80f10aa5475276eca2053
S2OHEE <s2h110@naver.com>
2024-08-16 18:06:21 +0900

    ✅ [Test] 테스트를 위해 사용한 코드나 주석 등 정리

--- vms_fe/src/components/companyInfoList.js
+++ vms_fe/src/components/companyInfoList.js
@@ -3,7 +3,7 @@ import React from "react";
 import CompanyItem from "./companyItem.js";
 import companyLogo1 from "../assets/images/company_logo_1.svg";
 
-// 테스트를 위해 mock데이터 넣음
+// 테스트를 위해 mock데이터 넣음( 삭제하거나 수정 가능 / 나중에 seed.js로 이미지도 같이 넣을예정 )
 const companies = [
   { logo: companyLogo1, name: "코드잇", category: "에듀테크" },
   { logo: "logo2.png", name: "기업명 2", category: "핀테크" },
