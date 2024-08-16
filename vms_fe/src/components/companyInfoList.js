5518d5109e0078fb9a6c9d1adf05762b4ffed30b
S2OHEE <s2h110@naver.com>
2024-08-16 15:43:27 +0900

    :sparkles: [Feat] companyList에 CompanyItem 컴포넌트 추가

--- vms_fe/src/components/companyInfoList.js
+++ vms_fe/src/components/companyInfoList.js
@@ -1,10 +1,12 @@
 import React from "react";
+import CompanyItem from "./companyItem.Js";
 
 const CompanyListSelect = () => {
   return (
     <div>
-      <div className="companyLogoName">기업로고 기업명 카테고리</div>
-      <button className="select">선택하기</button>
+      <CompanyItem />
     </div>
   );
 };
+
+export default CompanyListSelect;
