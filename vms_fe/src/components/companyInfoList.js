a7b8b43af845bff676b5929a65db0e2a83319490
S2OHEE <s2h110@naver.com>
2024-08-19 10:13:27 +0900

    ✅ [Test] mockData.js 파일을 사용해서 테스트

--- vms_fe/src/components/companyInfoList.js
+++ vms_fe/src/components/companyInfoList.js
@@ -1,21 +1,15 @@
 //여러 개의 CompanyItem 컴포넌트를 포함하는 컴포넌트
 import React from "react";
 import CompanyItem from "./companyItem.js";
-import companyLogo1 from "../assets/images/company_logo_1.svg";
-
-// 테스트를 위해 mock데이터 넣음( 삭제하거나 수정 가능 / 나중에 seed.js로 이미지도 같이 넣을예정 )
-const companies = [
-  { logo: companyLogo1, name: "코드잇", category: "에듀테크" },
-  { logo: "logo2.png", name: "기업명 2", category: "핀테크" },
-];
+import mockupData from "../assets/mock/mockData.js"; // mockData
 
 const CompanyListSelect = () => {
   return (
     <div className="company-list">
-      {companies.map((company, index) => (
+      {mockupData.map((company, index) => (
         <CompanyItem
           key={index}
-          logo={company.logo}
+          logo={company.logoUrl}
           name={company.name}
           category={company.category}
         />
