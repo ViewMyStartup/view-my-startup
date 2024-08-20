4fdac34e5c01d90aab7f8c643c2ea734d060d7fc
minseokim <claudia503@naver.com>
2024-08-20 11:10:57 +0900

    :sparkles: [Feat] PageNav 로고 위치에 맞춰 MyCompanyCompare 페이지의 heading 텍스트 위치를 동일하게 조정

--- vms_fe/src/pages/MyCompanyCompare.js
+++ vms_fe/src/pages/MyCompanyCompare.js
@@ -1,16 +1,17 @@
 import React from "react";
 import styles from "./MyCompanyCompare.module.css";
-
 import PageNav from "components/PageNav";
 
 function MyCompanyCompare() {
-
   return (
-    <div>
-      <h1>PageNav Component 테스트</h1>
+    <div className={styles.pageContainer}>
       <PageNav />
+      <div className={styles.content}>
+        <h1 className={styles.heading}>나의 기업을 선택해 주세요!</h1>
+      </div>
     </div>
   );
 }
 
 export default MyCompanyCompare;
+
