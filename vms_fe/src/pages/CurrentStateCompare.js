5018b97d309f073c9e5822a13083a5b3bb9474a8
Jun5 <chdh1005@gmail.com>
2024-08-20 15:17:54 +0900

    :sparkles: [Feat] CurrentStateCompare 페이지 작성 완료

--- vms_fe/src/pages/CurrentStateCompare.js
+++ vms_fe/src/pages/CurrentStateCompare.js
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./CurrentStateCompare.module.css";
-
 
 // 컴포넌트
 import PageNav from "components/PageNav";
-import DataRowSetRender from "components/DataRowSetRender"
+import DataRowSetRender from "components/DataRowSetRender";
 import Pagination from "components/common/Pagination";
-import Dropdown from "components/common/Dropdown";
 import DropdownMiddleSize from "components/common/DropdownMiddleSize";
 
 // 커스텀 훅
@@ -19,12 +17,15 @@ function CurrentStateCompare() {
   // CompanyPerRow & HeaderColumns 컴포넌트 테스트용 데이터 데이터
   const { currentPage, totalPages, handlePageChange } = usePageHandler();
 
+  const [sortOption, setSortOption] = useState([""]);
+
   const dataObject = {
     id: "1",
     rank: "3",
     name: "코딩마스터",
     img: Companyimg,
-    description: "코딩마스터는 청소년들을 위한 코딩 교육 플랫폼을 운영하는 기업입니다.",
+    description:
+      "코딩마스터는 청소년들을 위한 코딩 교육 플랫폼을 운영하는 기업입니다.",
     category: "에듀테크",
     total_investment: 12312959459,
     revenue: 3245204304,
@@ -33,22 +34,36 @@ function CurrentStateCompare() {
     investmentInfactTotal: 342534123124,
     myCompanyChooseCount: 124123,
     CompareChoohseCount: 12315565,
-    userName: "정준호", 
-    userRank: 3, 
-    userTotalInvestment: 3423401234, 
-    userComment: "테스트입니다." 
-  }
+    userName: "정준호",
+    userRank: 3,
+    userTotalInvestment: 3423401234,
+    userComment: "테스트입니다.",
+  };
 
   // 테스트용 데이터 세트
-  const dataList = [dataObject, dataObject, dataObject, dataObject, dataObject, dataObject, dataObject, dataObject, dataObject, dataObject ]
+  const dataList = [
+    dataObject,
+    dataObject,
+    dataObject,
+    dataObject,
+    dataObject,
+    dataObject,
+    dataObject,
+    dataObject,
+    dataObject,
+    dataObject,
+  ];
 
+  const handleOptionChange = (option) => {
+    setSortOption(option);
+  };
 
   const options = [
     "나의 기업 선택 횟수 높은순",
     "나의 기업 선택 횟수 낮은순",
     "실제 누적 투자 금액 높은순",
-    "실제 누적 투자 금액 낮은순"
-  ]
+    "실제 누적 투자 금액 낮은순",
+  ];
 
   return (
     <div className={styles.bgSet}>
@@ -58,14 +73,13 @@ function CurrentStateCompare() {
       <main className={styles.mainContainer}>
         <header className={styles.headerBox}>
           <span className={styles.headerText}>투자 현황</span>
-          <DropdownMiddleSize options={options}/>
+          <DropdownMiddleSize
+            options={options}
+            handleOptionChange={handleOptionChange}
+          />
         </header>
         <section>
-          <DataRowSetRender
-            type="invest"
-            dataList={dataList}
-
-          />
+          <DataRowSetRender type="invest" dataList={dataList} />
         </section>
       </main>
       <footer className={styles.footerSet}>
