797eead932879361a68876f2b84aa4ec8fd27b05
S2OHEE <s2h110@naver.com>
2024-08-21 13:12:31 +0900

    :wrench: [Chore] 사용하지 않는 코드 수정

--- vms_fe/src/components/common/CompanyDataPerRow.js
+++ vms_fe/src/components/common/CompanyDataPerRow.js
@@ -13,6 +13,10 @@ function CompanyDataPerRow({ type = "rank", dataObject = {} }) {
 
   //텍스트 자르기 (사전 작성)
   const truncateText = (text, maxLength) => {
+    // text가 문자열인지 확인하는 조건 추가(text가 undefined일 때 기본값을 제공)
+    if (typeof text !== "string") {
+      return "";
+    }
     if (text.length > maxLength) {
       return text.substring(0, maxLength) + "...";
     } else {
