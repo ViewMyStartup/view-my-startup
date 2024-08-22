fb59eb45f43e7efa6b564a2aceacc48292ced95a
jongmin <charlieko123@github.com>
2024-08-22 13:17:08 +0900

    :wrench:[Chore] 기업상세페이지 링크 추가

--- vms_fe/src/components/DataRowSetRender.js
+++ vms_fe/src/components/DataRowSetRender.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./DataRowSetRender.module.css";
 
 // 컴포넌트
@@ -7,15 +8,16 @@ import HeaderColumns from "./common/HeaderColumns";
 
 function DataRowSetRender({ type = "rank", dataList = [] }) {
   return (
-    <div className={styles.dataRowSet}> 
+    <div className={styles.dataRowSet}>
       <HeaderColumns type={type} />
       <ul>
         {dataList.map((data) => (
-          <CompanyDataPerRow type={type} dataObject={data} />
+          <Link key={data.name} to={`/id/${data.name}`}>
+            <CompanyDataPerRow type={type} dataObject={data} />
+          </Link>
         ))}
       </ul>
     </div>
-
   );
 }
 
--- vms_fe/src/pages/DefaultPage.js
+++ vms_fe/src/pages/DefaultPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import styles from "./DefaultPage.module.css";
 
 import PageNav from "components/PageNav.js";
