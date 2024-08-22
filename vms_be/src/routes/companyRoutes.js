c12e66c9e69b22e956a3cff65d09a1f52a1f47a9
S2OHEE <s2h110@naver.com>
2024-08-22 17:02:14 +0900

    ✨ [Feat] try catch문으로 서버 오류 발생시, 에러처리

--- vms_be/src/routes/companyRoutes.js
+++ vms_be/src/routes/companyRoutes.js
@@ -10,32 +10,39 @@ const router = express.Router();
 router.get("/:companyId", async (req, res) => {
   const { companyId } = req.params;
 
-  const company = await prisma.company.findUnique({
-    where: { id: parseInt(companyId) }, // companyId를 정수로 변환하여 조회
-    include: {
-      investments: true, // Investment 모델의 가상 투자정보 포함!
-    },
-  });
+  try {
+    const company = await prisma.company.findUnique({
+      where: { id: parseInt(companyId) }, // companyId를 정수로 변환하여 조회
+      include: {
+        investments: true, // Investment 모델의 가상 투자정보 포함!
+      },
+    });
 
-  // 해당 ID의 회사가 데이터베이스에 존재하지 않는 경우
-  if (!company) {
-    return res
-      .status(404)
-      .json({ error: "찾으시는 기업이 존재하지 않습니다." });
+    // 해당 ID의 회사가 데이터베이스에 존재하지 않는 경우
+    if (!company) {
+      return res
+        .status(404)
+        .json({ error: "찾으시는 기업이 존재하지 않습니다." });
+    }
+
+    // 투자 정보를 포함한 회사 정보 응답 형식
+    res.json({
+      id: company.id,
+      name: company.name,
+      logoUrl: company.logoUrl,
+      description: company.description,
+      category: company.category,
+      totalInvestment: company.totalInvestment,
+      revenue: company.revenue,
+      employees: company.employees,
+      investments: company.investments,
+    });
+  } catch (error) {
+    // 서버 오류 발생시
+    res
+      .status(500)
+      .json({ error: "기업 상세 정보를 가져오는 중 오류가 발생했습니다." });
   }
-
-  // 투자 정보를 포함한 회사 정보 응답 형식
-  res.json({
-    id: company.id,
-    name: company.name,
-    logoUrl: company.logoUrl,
-    description: company.description,
-    category: company.category,
-    totalInvestment: company.totalInvestment,
-    revenue: company.revenue,
-    employees: company.employees,
-    investments: company.investments,
-  });
 });
 
 export default router;
