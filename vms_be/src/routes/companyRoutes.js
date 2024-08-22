5f5738f32e7f570505afa565b24c63e25de184bb
S2OHEE <s2h110@naver.com>
2024-08-22 16:52:02 +0900

    ✨ [Feat] 기업 상세 조회 시 가상 투자 정보 포함

--- vms_be/src/routes/companyRoutes.js
+++ vms_be/src/routes/companyRoutes.js
@@ -7,6 +7,15 @@ const router = express.Router();
 // 기업 관련 API 작성
 
 // 기업 상세 조회 API
-router.get("/:companyId", async (req, res) => {});
+router.get("/:companyId", async (req, res) => {
+  const { companyId } = req.params;
+
+  const company = await prisma.company.findUnique({
+    where: { id: parseInt(companyId) }, // companyId를 정수로 변환하여 조회
+    include: {
+      investments: true, // Investment 모델의 가상 투자정보 포함!
+    },
+  });
+});
 
 export default router;
