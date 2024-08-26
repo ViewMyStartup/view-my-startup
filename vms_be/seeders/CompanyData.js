00eb8d5cac66259a24915ae73a7139705ed2f9a4
Jun5 <chdh1005@gmail.com>
2024-08-26 23:12:54 +0900

    :bug: [Fix] 이미지 제공을 위한 정적 경로가 /images로 설정되어 있지만, 시딩되는 파일도 /images로 중복되는 이슈로 api에서 이미지가 안보이는 이슈 수정

--- vms_be/seeders/CompanyData.js
+++ vms_be/seeders/CompanyData.js
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker/locale/ko';
 const companyData = [
   {
     name: "에듀넥스트",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "에듀넥스트는 현대 교육의 혁신을 선도하는 스타트업으로, 인공지능 기술을 활용하여 개인 맞춤형 학습 플랫폼을 제공합니다.\n\n" +
       "최근 교육 분야에서 인공지능의 중요성이 급부상함에 따라, 에듀넥스트는 학습자의 개별적인 학습 스타일과 속도에 맞춘 최적의 학습 경험을 설계하고 있습니다. 우리의 플랫폼은 최신 AI 알고리즘을 통해 학생들의 강점과 약점을 분석하고, 맞춤형 학습 경로를 제시하여 학습 효율성을 극대화합니다.\n\n" +
@@ -18,7 +18,7 @@ const companyData = [
   },
   {
     name: "코딩마스터",
-    logoUrl: "/images/logo2.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "코딩마스터는 청소년을 위한 코딩 교육에 특화된 플랫폼을 운영하는 기업으로, 미래의 디지털 리더를 양성하기 위해 최선을 다하고 있습니다.\n\n" +
       "코딩마스터는 컴퓨터 프로그래밍의 기초부터 고급 기술까지 포괄적인 교육 과정을 제공하며, 특히 실습 중심의 접근 방식을 채택하여 학습자들이 실제 문제를 해결하며 코딩 능력을 쌓을 수 있도록 돕습니다. 우리의 커리큘럼은 최신 기술 트렌드와 업계 요구에 맞춰 지속적으로 업데이트되며, 학생들이 글로벌 경쟁력을 갖출 수 있도록 지원합니다.\n\n" +
@@ -33,7 +33,7 @@ const companyData = [
   },
   {
     name: "러닝큐브",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "러닝큐브는 게이미피케이션을 기반으로 한 온라인 학습 플랫폼을 제공하여 학습의 재미와 효율성을 동시에 추구합니다.\n\n" +
       "우리는 전통적인 학습 방법에서 벗어나 학습자가 게임처럼 학습 목표를 달성할 수 있도록 다양한 도전 과제와 보상 시스템을 도입하고 있습니다. 이러한 접근 방식은 학습자의 참여를 유도하고, 자연스럽게 학습 동기를 부여하여 효과적인 학습을 가능하게 합니다.\n\n" +
@@ -48,7 +48,7 @@ const companyData = [
   },
   {
     name: "스터디온",
-    logoUrl: "/images/logo4.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "스터디온은 실시간 온라인 튜터링 서비스를 제공하는 선도적인 스타트업으로, 학생들에게 고품질의 학습 경험을 제공하기 위해 지속적으로 노력하고 있습니다.\n\n" +
       "우리는 다양한 분야의 전문 튜터와 실시간으로 연결하여 학생들이 필요한 시간에 맞춤형 지도를 받을 수 있도록 지원합니다. 이를 통해 학생들은 어려운 개념을 즉시 해결하고, 학습 과정에서 발생할 수 있는 문제를 실시간으로 해결할 수 있습니다.\n\n" +
@@ -63,7 +63,7 @@ const companyData = [
   },
   {
     name: "코드잇",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "코드잇은 '온라인 코딩 교육 서비스'를 운영하는 EdTech 스타트업입니다.\n\n" +
       "코딩 교육과 데이터 사이언스 교육에 대한 수요는 급격히 늘어나고 있지만, 아직까지 좋은 교육 서비스를 찾기란 쉽지 않습니다. 이를 해결하고자 코드잇은 모든 강의를 자체 제작하여 퀄리티 높은 콘텐츠를 제공하고, 동시에 코딩 교육에 최적화된 플랫폼을 개발하고 있습니다.\n\n" +
@@ -80,7 +80,7 @@ const companyData = [
   },
   {
     name: "헬스체크",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스체크는 개인 건강 관리와 예방을 위한 맞춤형 헬스케어 솔루션을 제공하는 스타트업입니다.\n\n" +
       "우리는 최첨단 기술을 활용하여 개인의 건강 상태를 실시간으로 모니터링하고, 필요한 조치를 취할 수 있는 솔루션을 제공합니다. 헬스체크는 스마트 웨어러블 기기와 모바일 애플리케이션을 통해 건강 데이터를 수집하고 분석하며, 사용자가 건강 목표를 효과적으로 달성할 수 있도록 돕습니다.\n\n" +
@@ -95,7 +95,7 @@ const companyData = [
   },
   {
     name: "메디브레인",
-    logoUrl: "/images/logo2.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "메디브레인은 인공지능을 활용한 진단 및 치료 지원 솔루션을 제공하는 헬스케어 스타트업입니다.\n\n" +
       "우리는 최신 AI 기술을 활용하여 의료 전문가들이 보다 정확하고 신속하게 진단과 치료 결정을 내릴 수 있도록 지원합니다. 메디브레인은 방대한 의료 데이터를 분석하여 질병의 조기 발견과 예방을 가능하게 하며, 효율적인 치료 계획을 수립하는 데 도움을 줍니다.\n\n" +
@@ -110,7 +110,7 @@ const companyData = [
   },
   {
     name: "핏라인",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "핏라인은 개인의 운동과 식단을 통합적으로 관리하는 플랫폼을 제공합니다.\n\n" +
       "우리는 사용자가 자신의 건강 목표를 설정하고, 이를 달성하기 위해 맞춤형 운동 및 식단 계획을 제안합니다. 핏라인은 실시간 데이터 추적과 분석 기능을 통해 사용자의 진행 상황을 모니터링하며, 필요한 조언과 피드백을 제공합니다.\n\n" +
@@ -125,7 +125,7 @@ const companyData = [
   },
   {
     name: "헬스아이",
-    logoUrl: "/images/logo4.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스아이는 스마트 건강 모니터링 장비와 애플리케이션을 제공하는 스타트업입니다.\n\n" +
       "우리는 최신 기술을 활용하여 사용자의 건강 상태를 실시간으로 모니터링하고, 건강 문제를 조기에 발견할 수 있는 솔루션을 제공합니다. 헬스아이는 사용자가 쉽게 접근할 수 있는 스마트 기기와 앱을 통해 건강 데이터를 분석하고, 필요한 정보를 제공합니다.\n\n" +
@@ -140,7 +140,7 @@ const companyData = [
   },
   {
     name: "다이어트클래스",
-    logoUrl: "/images/logo5.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "다이어트클래스는 체중 관리와 건강한 식습관 형성을 위한 종합 솔루션을 제공하는 플랫폼입니다.\n\n" +
       "우리는 개인 맞춤형 다이어트 계획과 식단 조절을 통해 사용자가 건강한 체중을 유지할 수 있도록 돕습니다. 다이어트클래스는 사용자의 신체 상태와 목표에 맞춘 체계적인 프로그램을 제공하며, 실시간 피드백과 지원을 통해 목표 달성을 지원합니다.\n\n" +
@@ -155,7 +155,7 @@ const companyData = [
   },
   {
     name: "에듀파크",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "에듀파크는 온라인 교육과 학습 관리 시스템을 제공하는 스타트업으로, 교육의 접근성과 효율성을 높이기 위해 노력하고 있습니다.\n\n" +
       "우리는 다양한 교육 콘텐츠와 기능을 갖춘 플랫폼을 통해 학습자들에게 최적의 학습 환경을 제공합니다. 에듀파크는 학습자의 필요에 맞춰 개별화된 학습 경로를 설계하고, 효율적인 학습 관리를 지원합니다.\n\n" +
@@ -170,7 +170,7 @@ const companyData = [
   },
   {
     name: "스마트스터디",
-    logoUrl: "/images/logo2.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "스마트스터디는 혁신적인 학습 도구와 자료를 제공하여 학생들의 학습 효과를 극대화하는 스타트업입니다.\n\n" +
       "우리는 최신 기술을 활용하여 학생들이 보다 효과적으로 학습할 수 있는 다양한 도구와 자료를 제공합니다. 스마트스터디는 개별 학습자의 요구에 맞춘 맞춤형 학습 경로를 설계하고, 다양한 학습 자원을 통해 학습의 효과를 높입니다.\n\n" +
@@ -185,7 +185,7 @@ const companyData = [
   },
   {
     name: "헬스프렌드",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스프렌드는 개인 건강 관리와 생활 습관 개선을 위한 맞춤형 솔루션을 제공합니다.\n\n" +
       "우리는 최신 헬스케어 기술을 활용하여 사용자의 건강 데이터를 실시간으로 분석하고, 건강 목표를 달성할 수 있는 계획을 제안합니다. 헬스프렌드는 스마트 기기와 애플리케이션을 통해 건강 관리의 전 과정을 지원하며, 사용자가 보다 건강한 생활을 유지할 수 있도록 돕습니다.\n\n" +
@@ -200,7 +200,7 @@ const companyData = [
   },
   {
     name: "테크에듀",
-    logoUrl: "/images/logo4.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "테크에듀는 최신 기술을 활용한 혁신적인 교육 솔루션을 제공하는 스타트업입니다.\n\n" +
       "우리는 교육의 효율성을 높이기 위해 다양한 기술적 접근 방식을 도입하고, 학습자의 요구에 맞춘 맞춤형 학습 경로를 설계합니다. 테크에듀는 최신 교육 기술을 통해 학습자의 참여와 성과를 극대화하며, 교육의 질을 향상시키는 것을 목표로 하고 있습니다.\n\n" +
@@ -215,7 +215,7 @@ const companyData = [
   },
   {
     name: "헬스온",
-    logoUrl: "/images/logo5.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스온은 개인의 건강 상태를 분석하고 개선하기 위한 종합 헬스케어 솔루션을 제공합니다.\n\n" +
       "우리는 사용자에게 맞춤형 건강 관리 계획을 제안하고, 실시간 데이터를 통해 건강 상태를 모니터링합니다. 헬스온은 최신 기술을 활용하여 건강 문제를 조기에 발견하고, 예방적인 조치를 취할 수 있도록 지원합니다.\n\n" +
@@ -230,7 +230,7 @@ const companyData = [
   },
   {
     name: "스마트러닝",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "스마트러닝은 첨단 기술을 활용하여 학습의 효율성을 높이는 교육 솔루션을 제공합니다.\n\n" +
       "우리는 인공지능과 데이터 분석을 통해 학습자의 필요에 맞춘 맞춤형 교육 콘텐츠를 제공하며, 학습 성과를 실시간으로 모니터링합니다. 스마트러닝은 학습자의 개별 요구에 맞춰 최적의 학습 경로를 설계하고, 효율적인 학습 환경을 조성하는 것을 목표로 하고 있습니다.\n\n" +
@@ -245,7 +245,7 @@ const companyData = [
   },
   {
     name: "헬스케어365",
-    logoUrl: "/images/logo2.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스케어365는 24/7 건강 모니터링 서비스를 제공하여 사용자의 건강을 지속적으로 관리합니다.\n\n" +
       "우리는 최신 헬스케어 기술을 활용하여 사용자의 건강 데이터를 실시간으로 분석하고, 필요에 따라 건강 관리 계획을 제안합니다. 헬스케어365는 스마트 기기와 애플리케이션을 통해 지속적인 건강 모니터링과 예방적 조치를 지원합니다.\n\n" +
@@ -260,7 +260,7 @@ const companyData = [
   },
   {
     name: "에듀프렌드",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "에듀프렌드는 개인 맞춤형 학습 계획을 통해 학생들의 학습 성과를 극대화하는 교육 스타트업입니다.\n\n" +
       "우리는 다양한 학습 도구와 자료를 제공하며, 학습자의 요구에 맞춘 맞춤형 학습 경로를 설계합니다. 에듀프렌드는 실시간 피드백과 분석 기능을 통해 학습자의 진행 상황을 모니터링하고, 필요한 지원을 적시에 제공하여 학습 목표 달성을 돕습니다.\n\n" +
@@ -275,7 +275,7 @@ const companyData = [
   },
   {
     name: "헬스매니저",
-    logoUrl: "/images/logo4.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스매니저는 개인 건강 관리와 체중 조절을 위한 종합 솔루션을 제공합니다.\n\n" +
       "우리는 사용자의 신체 상태와 목표에 맞춘 맞춤형 건강 관리 계획을 제안하며, 실시간 데이터를 통해 건강 상태를 모니터링합니다. 헬스매니저는 스마트 기기와 애플리케이션을 통해 건강 관리를 지원하며, 체중 조절과 건강 유지에 필요한 다양한 도구와 자료를 제공합니다.\n\n" +
@@ -290,7 +290,7 @@ const companyData = [
   },
   {
     name: "에듀마스터",
-    logoUrl: "/images/logo5.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "에듀마스터는 개인 맞춤형 학습 솔루션을 제공하여 학습자의 성과를 극대화하는 교육 스타트업입니다.\n\n" +
       "우리는 최신 기술을 활용하여 학습자의 개별 요구에 맞춘 맞춤형 교육 콘텐츠와 학습 경로를 설계합니다. 에듀마스터는 실시간 피드백과 분석 기능을 통해 학습자의 진행 상황을 모니터링하고, 필요한 지원을 적시에 제공하여 학습 목표 달성을 돕습니다.\n\n" +
@@ -305,7 +305,7 @@ const companyData = [
   },
   {
     name: "헬스케어존",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스케어존은 개인 건강 관리와 예방을 위한 종합 헬스케어 솔루션을 제공합니다.\n\n" +
       "우리는 최신 기술을 활용하여 사용자의 건강 상태를 실시간으로 분석하고, 건강 목표 달성을 위한 맞춤형 계획을 제안합니다. 헬스케어존은 스마트 기기와 애플리케이션을 통해 건강 관리를 지원하며, 사용자에게 필요한 건강 자료와 전문가의 조언을 제공합니다.\n\n" +
@@ -320,7 +320,7 @@ const companyData = [
   },
   {
     name: "테크노에듀",
-    logoUrl: "/images/logo2.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "테크노에듀는 최신 기술을 활용하여 혁신적인 교육 솔루션을 제공하는 스타트업입니다.\n\n" +
       "우리는 인공지능과 데이터 분석을 통해 학습자의 필요에 맞춘 맞춤형 교육 콘텐츠를 제공하며, 학습 성과를 실시간으로 모니터링합니다. 테크노에듀는 학습자의 개별 요구에 맞춰 최적의 학습 경로를 설계하고, 효율적인 학습 환경을 조성하는 것을 목표로 하고 있습니다.\n\n" +
@@ -335,7 +335,7 @@ const companyData = [
   },
   {
     name: "헬스케어랩",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스케어랩은 개인 건강 관리와 예방을 위한 혁신적인 헬스케어 솔루션을 제공합니다.\n\n" +
       "우리는 최신 헬스케어 기술을 활용하여 사용자의 건강 상태를 실시간으로 분석하고, 맞춤형 건강 관리 계획을 제안합니다. 헬스케어랩은 스마트 기기와 애플리케이션을 통해 지속적인 건강 모니터링과 예방적 조치를 지원합니다.\n\n" +
@@ -350,7 +350,7 @@ const companyData = [
   },
   {
     name: "헬스앤피트니스",
-    logoUrl: "/images/logo2.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스앤피트니스는 체중 감량과 피트니스 목표 달성을 위한 맞춤형 솔루션을 제공하는 스타트업입니다.\n\n" +
       "우리는 사용자의 체형과 목표에 맞춘 맞춤형 운동 및 식이 계획을 제공하며, 실시간으로 건강 상태를 모니터링합니다. 헬스앤피트니스는 스마트 기기와 애플리케이션을 통해 사용자가 목표를 달성할 수 있도록 지원하며, 체중 감량과 건강 유지를 위한 다양한 도구와 자료를 제공합니다.\n\n" +
@@ -374,13 +374,13 @@ const companyData = [
     totalInvestment: faker.number.int({ min: 1, max: 1000 })*10000000,
     revenue: faker.number.int({ min: 1, max: 1000 })*10000000,
     employees: faker.number.int({ min: 1, max: 100 }),
-    logoUrl: "/images/logo3.svg",
+    logoUrl: "/company_logo3.svg",
     mySelectionCount: faker.number.int({ min: 50, max: 500 }),
     CompareSelectionCount: faker.number.int({ min: 50, max: 500 }),
   },
   {
     name: "헬스플러스",
-    logoUrl: "/images/logo4.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스플러스는 건강 관리와 피트니스 목표 달성을 위한 종합 솔루션을 제공하는 스타트업입니다.\n\n" +
       "우리는 개인 맞춤형 운동 계획과 식이 요법을 통해 사용자가 건강한 생활을 유지할 수 있도록 돕습니다. 헬스플러스는 최신 헬스케어 기술을 활용하여 실시간으로 건강 상태를 모니터링하고, 목표 달성을 지원하는 다양한 도구와 자료를 제공합니다.\n\n" +
@@ -395,7 +395,7 @@ const companyData = [
   },
   {
     name: "코딩큐브",
-    logoUrl: "/images/logo5.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "코딩큐브는 혁신적인 코딩 교육을 제공하는 스타트업으로, 최신 기술을 활용한 실습 중심의 교육 과정을 운영하고 있습니다.\n\n" +
       "우리는 실습과 프로젝트 기반의 교육을 통해 학습자들이 코딩 능력을 실질적으로 향상시킬 수 있도록 지원합니다. 코딩큐브는 최신 기술 트렌드와 업계 요구에 맞춘 커리큘럼을 제공하며, 학생들이 실무에서 필요한 기술을 익히고, 실제 문제를 해결할 수 있는 능력을 키우는 데 중점을 두고 있습니다.\n\n" +
@@ -410,7 +410,7 @@ const companyData = [
   },
   {
     name: "에듀퓨처",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "에듀퓨처는 인공지능을 활용한 교육 콘텐츠 제작을 선도하는 스타트업입니다.\n\n" +
       "우리는 학습자의 성향과 요구에 맞춘 맞춤형 학습 콘텐츠를 제공하며, 실시간 피드백을 통해 학습 효과를 극대화합니다.\n\n" +
@@ -425,7 +425,7 @@ const companyData = [
   },
   {
     name: "헬스캔",
-    logoUrl: "/images/logo2.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "헬스캔은 개인의 건강 상태를 실시간으로 모니터링하고, 맞춤형 건강 솔루션을 제공하는 헬스케어 스타트업입니다.\n\n" +
       "우리는 스마트 기기와 AI 기술을 결합하여 사용자의 건강 데이터를 분석하고, 필요한 건강 관리 계획을 제안합니다.\n\n" +
@@ -440,7 +440,7 @@ const companyData = [
   },
   {
     name: "러닝플러스",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "러닝플러스는 교육과 게임을 결합한 혁신적인 학습 플랫폼을 제공하는 스타트업입니다.\n\n" +
       "우리는 학습자가 게임처럼 즐겁게 학습할 수 있는 환경을 조성하여 학습 동기를 높이고, 학습 성과를 향상시키고 있습니다.\n\n" +
@@ -455,7 +455,7 @@ const companyData = [
   },
   {
     name: "피트니스팜",
-    logoUrl: "/images/logo4.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "피트니스팜은 개인 맞춤형 운동 계획과 식단 관리를 제공하는 헬스케어 스타트업입니다.\n\n" +
       "우리는 사용자의 목표와 현재 상태에 맞춘 최적의 건강 관리 방법을 제안하며, 실시간 피드백과 트래킹 기능을 제공합니다.\n\n" +
@@ -470,7 +470,7 @@ const companyData = [
   },
   {
     name: "스마트팜랩",
-    logoUrl: "/images/logo5.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "스마트팜랩은 최첨단 기술을 활용하여 농업 생산성을 극대화하는 솔루션을 제공하는 스타트업입니다.\n\n" +
       "우리는 AI 기반의 작물 관리 시스템과 자동화된 재배 기술을 통해 농업 효율성을 높이고 있습니다.\n\n" +
@@ -485,7 +485,7 @@ const companyData = [
   },
   {
     name: "에코워터",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "에코워터는 혁신적인 수처리 기술을 개발하여 깨끗한 물을 제공하는 스타트업입니다.\n\n" +
       "우리는 효율적인 정수 시스템과 재활용 가능한 수처리 솔루션을 통해 환경 보호에 기여하고 있습니다.\n\n" +
@@ -500,7 +500,7 @@ const companyData = [
   },
   {
     name: "메디젠",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "메디젠은 유전자 분석을 통해 개인 맞춤형 의료 서비스를 제공하는 바이오테크 스타트업입니다.\n\n" +
       "우리는 최신 유전자 분석 기술을 활용하여 사용자의 건강 상태와 유전적 특성을 분석하고, 맞춤형 치료 계획을 수립합니다.\n\n" +
@@ -515,7 +515,7 @@ const companyData = [
   },
   {
     name: "클린에너지",
-    logoUrl: "/images/logo3.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "클린에너지는 친환경 에너지 솔루션을 제공하여 지속 가능한 에너지 미래를 선도하는 스타트업입니다.\n\n" +
       "우리는 태양광, 풍력 등 다양한 재생 가능 에너지를 활용하여 에너지 효율성을 높이고, 탄소 배출을 줄이는 데 기여하고 있습니다.\n\n" +
@@ -530,7 +530,7 @@ const companyData = [
   },
   {
     name: "모빌리티플러스",
-    logoUrl: "/images/logo4.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "모빌리티플러스는 자율 주행 기술을 개발하여 미래의 교통 시스템을 혁신하는 스타트업입니다.\n\n" +
       "우리는 자율 주행차의 안전성과 효율성을 높이기 위해 AI와 빅데이터 분석을 결합한 솔루션을 제공합니다.\n\n" +
@@ -545,7 +545,7 @@ const companyData = [
   },
   {
     name: "에어텍",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "에어텍은 차세대 항공 기술을 개발하여 항공 산업의 혁신을 이끄는 스타트업입니다.\n\n" +
       "우리는 효율적인 비행 시스템과 친환경 연료 기술을 통해 항공기의 성능을 향상시키고, 탄소 배출을 줄이는 데 주력하고 있습니다.\n\n" +
@@ -560,7 +560,7 @@ const companyData = [
   },
   {
     name: "그린팜",
-    logoUrl: "/images/logo1.svg",
+    logoUrl: `/company_logo${faker.number.int({ min: 1, max: 8 })}.svg`,
     description:
       "그린팜은 도시 농업 솔루션을 제공하여 도시의 식량 자급률을 높이는 스타트업입니다.\n\n" +
       "우리는 도시 내에서 지속 가능한 농업을 실현할 수 있는 스마트 농업 기술을 개발하고 있습니다.\n\n" +
