c0bf910776cf77b8b6f6f6e52ff2159e598e8ed5
Jun5 <chdh1005@gmail.com>
2024-08-26 19:27:58 +0900

    :sparkles: [Feat] investmentAmount가 추가되면, vmsInvestment 합계를 DB에서 연산하도록 트리거 기능 추가

--- vms_be/server.js
+++ vms_be/server.js
@@ -8,14 +8,28 @@ import companyRoutes from "./src/routes/companyRoutes.js";
 import investmentRoutes from "./src/routes/investmentRoutes.js";
 import errorHandlers from "./src/middlewares/errorHandler.js";
 
+//트리거
+import vmsInvestment from "./src/models/vmsInvestment.js";
+
+
 const app = express();
+
+vmsInvestment()
+  .then(() => {
+    console.log('데이터 베이스 세팅 완료');
+  })
+  .catch((err) => {
+    console.error('데이터 베이스 세팅 중 오류 발생:', err);
+  });
+
+
 app.use(express.json());
 
 // cors 설정( 일단, 모든 도메인에서의 요청을 허용해놓음 )
 app.use(cors());
 
 // 정적 파일 제공 설정
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'seeders')));
 
 // 라우터 등록( API 추가시, 라우터 추가 예정 )
 app.use("/api/companies", companyRoutes);
