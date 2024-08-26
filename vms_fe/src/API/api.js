6281557a69c97a1029d19bf205c29ee098716dad
jongmin <charlieko123@github.com>
2024-08-26 18:40:17 +0900

    :tada:[Init] axios 설치

--- vms_fe/src/API/api.js
+++ vms_fe/src/API/api.js
@@ -7,5 +7,16 @@ console.log("apiUrl:", apiUrl);
 // 아래 API 호출 코드 작성
 
 // 기업 전체 리스트 조회 API
-
+export const fetchCompanies = async (params) => {
+  const response = await axios.get(`${apiUrl}/companies`, {
+    params: {
+      page: params.page,
+      limit: params.limit,
+      search: params.search,
+      sort_by: params.sortBy,
+      order: params.order,
+    },
+  });
+  return response.data;
+};
 // 기업 상세 조회 API
