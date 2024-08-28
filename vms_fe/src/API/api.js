7e3828e58ff74b34b54d5a6e93cdd8932762658a
minseokim <claudia503@naver.com>
2024-08-28 09:48:16 +0900

    :sparkles: [Feat] api.js 파일에서 서버에서 가져온 기업 데이터를 정렬하는 sortCompanies 함수 추가

--- vms_fe/src/API/api.js
+++ vms_fe/src/API/api.js
@@ -23,11 +23,45 @@ export async function getApiData(page, limit, search, sort_by, order) {
   }
 }
 
+// 서버에서 가져온 기업 데이터
 export const fetchCompanies = async () => {
   if (!apiUrl) {
     throw new Error("API URL이 설정되지 않았습니다.");
   }
   const response = await axios.get(`${apiUrl}/api/companies`);
   return response.data;
+};
+
+// 서버에서 가져온 기업 데이터를 정렬하는 sortCompanies
+export const sortCompanies = (companies, option) => {
+  const sortedList = [...companies];
+
+  switch (option) {
+    case "누적 투자금액 높은순":
+      sortedList.sort((a, b) => b.totalInvestment - a.totalInvestment);
+      break;
+    case "누적 투자금액 낮은순":
+      sortedList.sort((a, b) => a.totalInvestment - b.totalInvestment);
+      break;
+    case "매출액 높은순":
+      sortedList.sort((a, b) => b.revenue - a.revenue);
+      break;
+    case "매출액 낮은순":
+      sortedList.sort((a, b) => a.revenue - b.revenue);
+      break;
+    case "고용 인원 많은순":
+      sortedList.sort((a, b) => b.employees - a.employees);
+      break;
+    case "고용 인원 적은순":
+      sortedList.sort((a, b) => a.employees - b.employees);
+      break;
+    default:
+      break;
+  }
+
+  return sortedList.map((company, index) => ({
+    ...company,
+    rank: index + 1,
+  }));
 };
 
