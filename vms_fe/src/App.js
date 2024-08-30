9e48c2963b675499d6fbb39c25e7110a25632105
Kim HyunU <recom54@gmail.com>
2024-08-30 17:43:46 +0900

    🔧 [Chore] 랜딩페이지 경로변경

--- vms_fe/src/App.js
+++ vms_fe/src/App.js
@@ -9,19 +9,19 @@ import CompanyInvestDetail from "pages/CompanyInvestDetail";
 import CurrentStateInvest from "pages/CurrentStateInvest";
 import MyCompanyCompare from "pages/MyCompanyCompare";
 import CurrentStateCompare from "pages/CurrentStateCompare";
-import MainPage from "pages/MainPage";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<Navigate to="/" />} />
-        <Route exact path="/" element={<DefaultPage />} />
+        <Route exact path="/mainpage" element={<DefaultPage />} />
         <Route path="/mycompany" element={<MyCompanyCompare />} />
         <Route path="/compare" element={<CurrentStateCompare />} />
         <Route path="/investinfo" element={<CurrentStateInvest />} />
         <Route path="/id/:companyId" element={<CompanyInvestDetail />} />
-        <Route path="/mainpage" element={<MainPage />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
